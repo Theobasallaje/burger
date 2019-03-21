8888a7da46a0310d5955aeb7265e43d04f9d919b
Theo <theobasallaje@gmail.com>
2019-03-20 22:00:39 -0500

    some more intial files

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE `burger` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `burger_name` VARCHAR( 255) NOT NULL,
+  `devoured` BOOLEAN,
+
+  PRIMARY KEY ( `id` ) 
+);
